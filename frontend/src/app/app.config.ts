a43f071dc19b732492e7cb5c4a7e767a1d16e470
supernova0735 <mariofrodm@gmail.com>
2026-06-07 20:54:45 -0600

    chore: habilitar fetch en HttpClient para SSR

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,8 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient()
+    provideHttpClient(withFetch())
   ]
 };
